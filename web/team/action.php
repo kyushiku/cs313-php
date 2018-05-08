6dae1d95596834605925eb6fef9f4fe5ebc498ad
Kate <car13044@byui.edu>
2018-05-08 16:09:13 -0600

    team work

---
+++ web/team/action.php
@@ -0,0 +1,6 @@
+<!DOCTYPE>
+<html>
+    <?php
+    echo $_POST["name"];
+    ?>
+</html>
---
+++ web/team/team.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+	<head>
+
+	</head>
+	<form action="action.php" method="post">
+        <div>Name</div>
+		<input name="name" type="text"> <br>
+        <div>Email</div>
+		<input name="email" type="email"> <br>
+        
+		<input type="radio" name="class"> CS <br>
+		<input type="radio" name="class"> WD <br>
+		<input type="radio" name="class"> CIT <br>
+		<input type="radio" name="class"> CE <br>
+		<textarea rows="4" cols="50">
+		</textarea>
+        <input type="submit">
+	</form>
+</html>
