c048250d3e4a3df29c814554d78fee323a7e8066
Kate <car13044@byui.edu>
2018-05-08 15:56:05 -0600

    team work

---
+++ web/team.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+	<head>
+
+	</head>
+	<form>
+		<input id="name" type="text"> <br>
+		<input id="email" type="email"> <br>
+		<input type="radio"> CS <br>
+		<input type="radio"> WD <br>
+		<input type="radio"> CIT <br>
+		<input type="radio"> CE <br>
+		<textarea rows="4" cols="50">
+		</textarea>
+	</form>
+</html>
