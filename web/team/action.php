fbc58b5e44cd1bf1d8c822e4202252d676c3ae50
Kate <car13044@byui.edu>
2018-05-08 16:40:45 -0600

    this

--- web/team/action.php
+++ web/team/action.php
@@ -1,8 +1,10 @@
 <!DOCTYPE>
 <html>
    <?php
+    
+    $email = $_POST["email"];
     echo $_POST["name"];
-    echo "<a href=\"mailto:" . $_POST["email"] . "\">mail</a>"
+    echo '<a href="mailto:  $email "> </a> '
     echo $_POST["class"];
     echo $_POST["comments"];
     
