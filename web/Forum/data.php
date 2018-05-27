c7750526976bab65962cbad5fbfa844637cd7899
Kate <car13044@byui.edu>
2018-05-26 20:33:08 -0600

    adding uikit

--- web/Forum/data.php
+++ web/Forum/data.php
@@ -64,17 +64,8 @@ catch (PDOException $ex)
     <div class="uk-card-header">
         <div class="uk-grid-small uk-flex-middle" uk-grid>
             <div class="uk-width-expand">
-                <h3 class="uk-card-title uk-margin-remove-bottom">
-                <?php
-                foreach ($db->query('SELECT title FROM threads') as $thread)
-                {
-                    $title = $thread["desc_text"];
-                    if($title == "UPDATE 1.01")
-                    {
-                    echo $text;
-                    }
-                }
-                ?></h3>
+                <h3 class="uk-card-title uk-margin-remove-bottom"> This is title
+              </h3>
                 <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
             </div>
         </div>
