890fd2ce29bb1aeea7a01966569968c398cd8005
Kate <car13044@byui.edu>
2018-05-26 19:48:30 -0600

    adding uikit

--- web/Forum/data.php
+++ web/Forum/data.php
@@ -60,7 +60,7 @@ catch (PDOException $ex)
     </div>
 </nav>
 
- <div class="uk-card uk-card-default uk-width-1-2@m">
+ <div class="uk-card uk-card-default">
     <div class="uk-card-header">
         <div class="uk-grid-small uk-flex-middle" uk-grid>
             <div class="uk-width-expand">
@@ -73,7 +73,7 @@ catch (PDOException $ex)
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
     </div>
     <div class="uk-card-footer">
-        <a href="#" class="uk-button uk-button-text">Read more</a>
+        <a href="#" class="uk-button uk-button-text">Comments</a>
     </div>
 </div>
     
