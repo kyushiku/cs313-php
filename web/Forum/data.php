3eeffc6c2ae44d24be40403d2e689cbe88da2005
Kate <car13044@byui.edu>
2018-05-26 19:23:08 -0600

    adding uikit

--- web/Forum/data.php
+++ web/Forum/data.php
@@ -33,7 +33,7 @@ catch (PDOException $ex)
         foreach ($db->query('SELECT name FROM users') as $users)
         {
         $name = $users["name"];
-        echo "<p>$name</p>";
+        echo "<div "uk-navbar-right">$name</div>";
         }
     ?>
     </div>
