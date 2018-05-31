870e081b8a18ecdd94c0d7e01251a05e39b5689a
kate <car13044@byui.edu>
2018-05-31 15:53:52 -0600

    testing soemthing from class

--- web/Forum/data.php
+++ web/Forum/data.php
@@ -1,23 +1,10 @@
 <?php
-try
-{
-    $dbUrl = getenv('DATABASE_URL');
-    $dbopts = parse_url($dbUrl);
-    
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
-    
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+require("db.php");
+$db = get_db();
+
+$statement = $db->prepare($query);
+// Bind any variables i need here
+$statement->execute()->fetchALL(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
