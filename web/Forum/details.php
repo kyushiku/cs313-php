2916a65bc80271e4e88eb5d233da08e0677f0afd
kate <car13044@byui.edu>
2018-06-04 17:46:48 -0600

    testing soemthing from class

--- web/Forum/details.php
+++ web/Forum/details.php
@@ -9,14 +9,14 @@ $title = htmlspecialchars($_GET["threads_id"]);
 	<title></title>
 </head>
 <body>
-
 <form action="insert.php" method="POST">
 <input type="hidden" name="threads_id" value="<?php echo $title; ?>">
+<input type="text" name="title">
 <input type="date" name="date"><br>
 <textarea name="content" placeholder="Content"></textarea>
 
 <br><br>
-<input type="submit" value="Add Note">
+<input type="submit" value="Add Thread">
 </form>
 
 </body>
