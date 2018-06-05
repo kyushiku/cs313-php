a0b1740edc358c1d35732f3837b2e47d20224ab6
kate <car13044@byui.edu>
2018-06-05 17:10:58 -0600

    testing soemthing from class

--- web/Forum/create_user.php
+++ web/Forum/create_user.php
@@ -7,14 +7,16 @@ require("db.php");
 <head></head>
 <body>
 <?php
-$user = htmlspecialchars($_POST["username"]);
+$username = htmlspecialchars($_POST["username"]);
+$user = htmlspecialchars($_POST["user"]);
 $pass = htmlspecialchars($_POST["pass"]);
 $passwordHash = password_hash($pass, PASSWORD_DEFAULT);
 
 $db = get_db();
-$query = "INSERT INTO users (username, pass) VALUES (:username, :pass)";
+$query = "INSERT INTO users (username, user, pass) VALUES (:username, :user, :pass)";
 $statement = $db->prepare($query);
-$statement->bindValue(":username", $user, PDO::PARAM_STR);
+$statement->bindValue(":username", $username, PDO::PARAM_STR);
+$statement->bindValue(":user", $user, PDO::PARAM_STR);
 $statement->bindValue(":pass", $pass, PDO::PARAM_STR);
 $statement->execute();
 header("Location: data.php");
