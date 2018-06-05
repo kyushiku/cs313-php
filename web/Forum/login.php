7c93783bd2ead5a76e23ee01e5f5608d5fd27cac
kate <car13044@byui.edu>
2018-06-05 17:03:50 -0600

    testing soemthing from class

--- web/Forum/login.php
+++ web/Forum/login.php
@@ -5,10 +5,18 @@ $user = htmlspecialchars($_GET["users_id"]);
 ?>
 <!DOCTYPE>
 <html>
+<head>
+<script src="js/uikit.min.js"></script>
+<script src="js/uikit-icons.min.js"></script>
+<link rel="stylesheet" href="css/uikit-rtl.min.css" type="text/css">
+</head>
+
 <form action="create_user.php" method="POST">
 <input type="text" name="username"><br>
 <input type="text" name="pass"><br>
 
 <input type="submit" value="Add User">
 </form>
+<a href='register.php' class='uk-button uk-button-text'>Register Here!</a>
+
 </html>
