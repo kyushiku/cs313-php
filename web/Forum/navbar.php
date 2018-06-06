fd0a563a3c42247ec17b0153c834481cb37a9c30
kate <car13044@byui.edu>
2018-06-05 21:21:35 -0600

    testing soemthing from class

---
+++ web/Forum/navbar.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE>
+<html>
+<head>
+<head>
+   <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+    <link rel="stylesheet" href="css/uikit-rtl.min.css" type="text/css">
+</head>
+</head>
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+            <li>
+                <a href="#">News</a>
+                <div class="uk-navbar-dropdown">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li class="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Troubleshoot</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="#">Troubleshooting</a></li>
+            </ul>
+        </div>
+        <div class ="uk-navbar-right">
+            <ul class="uk-navbar-nav">
+                <li>
+                <a href="details.php">New Thread</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</html>
--- web/Forum/register.php
+++ web/Forum/register.php
@@ -10,7 +10,7 @@ $user = htmlspecialchars($_GET["users_id"]);
     <script src="js/uikit-icons.min.js"></script>
     <link rel="stylesheet" href="css/uikit-rtl.min.css" type="text/css">
 </head>
-
+<?php require("navbar.php") ?>
 <form action="create_user.php" method="POST">
 <div>Username:</div><input type="text" name="username"><br>
 <div>First and Last name:</div><input type="text" name="name"><br>
