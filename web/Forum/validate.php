f4363861d4522d380491351f1c1467aed57586f3
kate <car13044@byui.edu>
2018-06-11 21:24:06 -0600

    adding user

--- web/Forum/validate.php
+++ web/Forum/validate.php
@@ -36,10 +36,7 @@ $statement->bindValue(':password', $hashedPassword);
 $statement->execute();
 // finally, redirect them to the sign in page
 header("Location: login.php");
-die(); // we always include a die after redirects. In this case, there would be no
-       // harm if the user got the rest of the page, because there is nothing else
-       // but in general, there could be things after here that we don't want them
-       // to see.
+die(); 
 ?>
 </body>
 
