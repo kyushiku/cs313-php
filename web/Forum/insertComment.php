28043716f610b042484b722e6cdfd507bbe7b4db
kate <car13044@byui.edu>
2018-06-12 00:00:16 -0600

    stop

--- web/Forum/insertComment.php
+++ web/Forum/insertComment.php
@@ -14,16 +14,23 @@ else
     }
     else
     {
-        //a real user posted a real reply
-        $sql = "INSERT INTO 
-                    comments(comm_text,
-                          users_id,
-                          threads_id,
-                          comment_date) 
-                VALUES ('" . $_POST['comm_text'] . "',
-                    " . $_SESSION['username'] . ",
-                        " . $_POST['threads_id'] . ",
-                        NOW()";
+    $text = htmlspecialchars($_POST["comm_text"]);
+    $user_id = htmlspecialchars($_POST["users_id"]);
+    $thread_id = htmlspecialchars($_POST["threads_id"]);
+    $date = NOW();
+
+    require("db.php");
+    $db = get_db();
+    $query = "INSERT INTO comments (comm_text, users_id, threads_id, comment_date) VALUES (:text, :user_id, :thread_id, :date)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(":text", $text, PDO::PARAM_STR);
+    $statement->bindValue(":user_id", $user_id, PDO::PARAM_STR);
+    $statement->bindValue(":thread_id", $thread_id, PDO::PARAM_INT);
+    $statement->bindValue(":date", $date, PDO::PARAM_STR);
+    $statement->execute();
+    header("Location: data.php");
+    die();
     }
+
 }
  ?>
