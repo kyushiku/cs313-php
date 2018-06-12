c40294216361c41b4cd1725c65eb0d498d056d24
kate <car13044@byui.edu>
2018-06-12 15:31:40 -0600

    stop

--- web/Forum/thread.php
+++ web/Forum/thread.php
@@ -14,10 +14,10 @@ $title = htmlspecialchars($_GET["threads_id"]);
 <body>
 <form action="insert.php" method="POST">
 <input type="hidden" name="threads_id" value="<?php echo $title; ?>">
-<input type="text" name="title"><br>
-<input type="radio" name="categories_id" value ="1"><br>
-<input type="radio" name="categories_id" value ="2"><br>
-<textarea name="desc_text" placeholder="Content"></textarea>
+Title:<input type="text" name="title"><br>
+News:<input type="radio" name="categories_id" value ="1"><br>
+Troubleshooting:<input type="radio" name="categories_id" value ="2"><br>
+Content:<textarea name="desc_text" placeholder="Content"></textarea>
 
 <br><br>
 <input type="submit" value="Add Thread">
