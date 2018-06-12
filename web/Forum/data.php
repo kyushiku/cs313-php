2f8fd76455f3253e1e982c3b32592e6da6f17fda
kate <car13044@byui.edu>
2018-06-12 13:01:03 -0600

    stop

--- web/Forum/data.php
+++ web/Forum/data.php
@@ -81,7 +81,7 @@ echo "<div class='uk-card uk-card-default'>
     <div class='uk-card-footer'>
         <form action='comment.php method = 'POST'>
         <input type='hidden' name='threads_id' value='$thread_id'>
-        <type = 'submit' value = 'Comments'>
+        <input type = 'submit' value = 'Comments' class ='uk-button uk-button-text'>
         </form>
     </div>
 </div>";
