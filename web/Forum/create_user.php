7130a53a5c695d15c8c70dff741d338dea711917
kate <car13044@byui.edu>
2018-06-11 20:54:50 -0600

    adding user

--- web/Forum/create_user.php
+++ web/Forum/create_user.php
@@ -8,15 +8,13 @@ require("db.php");
 <body>
 <?php
 $username = htmlspecialchars($_POST["username"]);
-$name = htmlspecialchars($_POST["name"]);
 $pass = htmlspecialchars($_POST["pass"]);
 $passwordHash = password_hash($pass, PASSWORD_DEFAULT);
 
 $db = get_db();
-$query = "INSERT INTO users (username, name, pass) VALUES (:username, :name, :pass)";
+$query = "INSERT INTO users (username, pass) VALUES (:username, :pass)";
 $statement = $db->prepare($query);
 $statement->bindValue(":username", $username, PDO::PARAM_STR);
-$statement->bindValue(":name", $name, PDO::PARAM_STR);
 $statement->bindValue(":pass", $passwordHash, PDO::PARAM_STR);
 $statement->execute();
 header("Location: data.php");
