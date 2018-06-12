16c57eb4b156b32432734d324860762c886888ef
kate <car13044@byui.edu>
2018-06-12 00:11:48 -0600

    stop

--- web/Forum/insertComment.php
+++ web/Forum/insertComment.php
@@ -26,8 +26,8 @@ else
     $statement = $db->prepare($query);
     $statement->bindValue(":text", $text, PDO::PARAM_STR);
     $statement->bindValue(":user_id", $user_id, PDO::PARAM_STR);
-    $statement->bindValue(":thread_id", $thread_id, PDO::PARAM_STR);
-    $statement->bindValue(":date", $date, PDO::PARAM_STR);
+    $statement->bindValue(":thread_id", $thread_id, PDO::PARAM_INT);
+    $statement->bindValue(":date", $date, PDO::PARAM_INT);
     $statement->execute();
     header("Location: data.php");
     die();
